d7b9019fc269acd51045a2fa61597ec5f10d6130
张飞 <admin@xuexi.cn>
2021-08-29 00:34:39 +0800

    增加输出重定向

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git -C /xuexi/code/TechXueXi config pull.ff only
 touch /var/log/cron.log
-./run.sh &
+./run.sh 2>&1 &
 echo -e "$CRONTIME $USER /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" > /etc/cron.d/mycron
 crontab /etc/cron.d/mycron
 cron && tail -f /var/log/cron.log
