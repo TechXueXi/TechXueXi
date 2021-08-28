6f1a25f4bdf706bbc0632168789ddcc8654ed938
张飞 <admin@xuexi.cn>
2021-08-28 17:38:40 +0800

    修改docker脚本

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-cd code
-cd TechXueXi
+cd /xuexi/code/TechXueXi
 echo "检查更新"
 git pull
 echo "检查更新完毕"
-cd ..
-cp -r TechXueXi/SourcePackages/* ..
-cd ..
+cp -r /xuexi/code/TechXueXi/SourcePackages/* /xuexi
+cd /xuexi/code
+echo "开始运行"
 python pandalearning.py > /proc/1/fd/1
