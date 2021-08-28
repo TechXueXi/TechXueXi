fcaece570e94f3d14e2c74e054f6fd3df9f18b4f
张飞 <admin@xuexi.cn>
2021-08-29 00:30:17 +0800

    修改用户

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 git -C /xuexi/code/TechXueXi config pull.ff only
 touch /var/log/cron.log
 ./run.sh &
-echo -e "$CRONTIME /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" > /etc/cron.d/mycron
+echo -e "$CRONTIME $USER /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" > /etc/cron.d/mycron
 crontab /etc/cron.d/mycron
 cron && tail -f /var/log/cron.log
