c00a3cbaf73240f3b7fa1464befda3883a876b57
张飞 <admin@xuexi.cn>
2021-08-28 23:28:14 +0800

    使用绝对路径

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-cd /xuexi/code/TechXueXi
 echo "检查更新"
-git pull $Sourcepath $pullbranche
+git -C /xuexi/code/TechXueXi pull $Sourcepath $pullbranche
 echo "检查更新完毕"
 cp -r /xuexi/code/TechXueXi/SourcePackages/* /xuexi
 echo "开始运行"
-cd /xuexi
-python ./pandalearning.py
+python /xuexi/pandalearning.py
