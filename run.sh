8cfef7d925ab535f67097fc5e666b8cb5a29938d
张飞 <admin@xuexi.cn>
2021-08-29 00:00:25 +0800

    修改run

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git -C /xuexi/code/TechXueXi pull $Sourcepath $pullbranche
 echo "检查更新完毕"
 cp -r /xuexi/code/TechXueXi/SourcePackages/* /xuexi
 echo "开始运行"
-python /xuexi/pandalearning.py
+/usr/local/bin/python /xuexi/pandalearning.py
