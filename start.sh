4471d7babd58a926f1d14a4faaa5be647252fa71
张飞 <admin@xuexi.cn>
2021-08-28 22:56:22 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-./run.sh > /proc/1/fd/1 &
-echo -e "$CRONTIME /xuexi/run.sh > /proc/1/fd/1\n#empty line" > /etc/cron.d/mycron
+touch /var/log/cron.log
+./run.sh >> /var/log/cron.log 2>&1 &
+echo -e "$CRONTIME /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" > /etc/cron.d/mycron
 crontab /etc/cron.d/mycron
-cron -f
+cron && tail -f /var/log/cron.log
