77d3305a22d1753e4f713c7e76fd31e8a856f0a5
张飞 <admin@xuexi.cn>
2021-08-29 08:10:51 +0800

    修正启动参数

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git -C /xuexi/code/TechXueXi config pull.ff only
+printenv >> /etc/environment
 touch /var/log/cron.log
 ./run.sh 2>&1 &
 echo -e "$CRONTIME $USER /xuexi/run.sh >> /var/log/cron.log 2>&1\n#empty line" > /etc/cron.d/mycron
