1e0447720bcc451c598f58b67acf56e7fe5f8e41
dompling <374779789@qq.com>
2021-10-13 15:03:32 +0800

    学习强国

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ git -C /xuexi/code/TechXueXi pull $Sourcepath $pullbranche
 echo "检查更新完毕"
 cp -r /xuexi/code/TechXueXi/SourcePackages/* /xuexi
 echo "开始运行"
-nohup python3 /xuexi/pandalearning.py >> script_log.log 2>&1& exit 0
+nohup python3 /xuexi/pandalearning.py >> /xuexi/log/script_log.log 2>&1& exit 0
