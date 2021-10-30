f9e4fb2bcf30dd08140e46f3becbc81be2b292af
nineja5340 <nineja5340@datakop.com>
2021-10-30 09:51:54 +0800

    测试supervisorctl

--- dev_build.sh
+++ dev_build.sh
@@ -4,7 +4,7 @@ if [[ -z ${PROXY_SERVER} ]]; then
     echo "未设置代理服务 \${PROXY_SERVER}"
 elif curl -o /dev/null -k -s -S -x ${PROXY_SERVER} --connect-timeout 5 https://github.com; then
     echo "使用代理服务器 \${PROXY_SERVER}:${PROXY_SERVER}"
-    build_cmd="--build-arg ALL_PROXY=${PROXY_SERVER} \
+    build_use_proxy="--build-arg ALL_PROXY=${PROXY_SERVER} \
       --build-arg HTTP_PROXY=${PROXY_SERVER} \
       --build-arg USE_PROXY=on \
       --build-arg all_proxy=${PROXY_SERVER} \
@@ -16,11 +16,11 @@ else
     echo "请检查代理服务 \${PROXY_SERVER}:${PROXY_SERVER}"
 fi
 
-# ${ varname :- word } ：如果varname存在并且不为null，返回varname的值，否则返回word。
-
 TAG='Alpha'
-
+Sourcepath=https://github.com/nineja5340/TechXueXi.git
 docker build \
-    ${build_cmd:-} \
+    ${build_use_proxy:-} \
+    --build-arg Sourcepath=${Sourcepath} \
+    --shm-size 2g \
     --tag techxuexi/techxuexi-amd64:${TAG:-'dev'} \
     https://github.com/nineja5340/TechXueXi.git
