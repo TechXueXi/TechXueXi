8a4a3ad955839f4475679035b21b1930635cf861
nineja5340 <nineja5340@datakop.com>
2021-10-30 11:14:30 +0800

    优化编译选项,便于本地编译
    优化编译选项,便于本地编译

--- dev_build.sh
+++ dev_build.sh
@@ -1,10 +1,11 @@
-PROXY_SERVER="http://192.168.168.121:21086"
+#!/bin/bash
 
-if [[ -z ${PROXY_SERVER} ]]; then
-    echo "未设置代理服务 \${PROXY_SERVER}"
-elif curl -o /dev/null -k -s -S -x ${PROXY_SERVER} --connect-timeout 5 https://github.com; then
-    echo "使用代理服务器 \${PROXY_SERVER}:${PROXY_SERVER}"
-    build_use_proxy="--build-arg ALL_PROXY=${PROXY_SERVER} \
+init_proxy_server() {
+    if [[ -z ${PROXY_SERVER} ]]; then
+        echo "未设置代理服务 \${PROXY_SERVER}"
+    elif curl -o /dev/null -k -s -S -x ${PROXY_SERVER} --connect-timeout 5 https://github.com; then
+        echo "使用代理服务器 \${PROXY_SERVER}:${PROXY_SERVER}"
+        build_use_proxy="--build-arg ALL_PROXY=${PROXY_SERVER} \
       --build-arg HTTP_PROXY=${PROXY_SERVER} \
       --build-arg USE_PROXY=on \
       --build-arg all_proxy=${PROXY_SERVER} \
@@ -12,12 +13,17 @@ elif curl -o /dev/null -k -s -S -x ${PROXY_SERVER} --connect-timeout 5 https://g
       --build-arg use_proxy=on \
       --build-arg GO111MODULE=on \
       --build-arg GOPROXY=https://goproxy.io"
-else
-    echo "请检查代理服务 \${PROXY_SERVER}:${PROXY_SERVER}"
-fi
+    else
+        echo "请检查代理服务 \${PROXY_SERVER}:${PROXY_SERVER}"
+    fi
+}
 
+PROXY_SERVER=$HTTP_PROXY_SERVER
 Sourcepath=https://github.com/TechXueXi/TechXueXi.git
 usebranche='dev'
+
+init_proxy_server
+
 docker build \
     ${build_use_proxy:-} \
     --build-arg Sourcepath=${Sourcepath} \
