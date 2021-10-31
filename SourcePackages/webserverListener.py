b13ecddc98347dccbbc17073691fa915f8a9cf9f
techxuexi-web <techxuexi@yandex.com>
2021-10-31 11:23:13 +0800

    修复了循环引用的问题~

--- SourcePackages/webserverListener.py
+++ SourcePackages/webserverListener.py
@@ -85,6 +85,7 @@ def web_log(send_log):
 def hello_world():
     return redirect('/static/index.html', code=302)
 
+
 @app.route('/jump')
 def hello_world():
     return redirect('/static/index.html', code=302)
@@ -226,7 +227,6 @@ def list_messages():
             web_db.session.delete(message)
     web_db.session.commit()
     return resp_models_ok(messages)
-
 
 
 if __name__ == "__main__":
