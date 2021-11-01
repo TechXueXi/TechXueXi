8edff98f9305a3fa072551db00be90388e020a5d
techxuexi-web <techxuexi@yandex.com>
2021-11-01 12:03:14 +0800

    fix bug

--- docker-manifest.sh
+++ docker-manifest.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
 TAG='latest'
 PUBLIC_REGISTRY_URL='docker.io'
 PRIVATE_REGISTRY_URL='docker.io'
 PUBLIC_COMMUNITY_USER='techxuexi'
 PRIVATE_COMMUNITY_USER='techxuexi'
 IMAGE_NAME='techxuexi'
-
 
 LOG_INFO() {
   echo -e "\033[0;32m[INFO] $* \033[0m"
@@ -21,22 +19,37 @@ LOG_WARNING() {
 
 # 输出命令到日志并运行
 LOGGER_RUN() {
-    LOG_INFO "$*"
-    bash -c "$*"
+  LOG_INFO "$*"
+  bash -c "$*"
 }
 
 if [[ $1 ]]; then
-    PRIVATE_REGISTRY_URL=$1
+  PRIVATE_REGISTRY_URL=$1
 fi
 
 if [[ $2 ]]; then
-    PRIVATE_COMMUNITY_USER=$2
+  PRIVATE_COMMUNITY_USER=$2
 fi
 
 OUT_TAGS=""
 for PLATFORM in amd64 arm64v8 arm32v7; do
-    LOGGER_RUN docker pull --platform ${PLATFORM} ${PUBLIC_REGISTRY_URL}/${PUBLIC_COMMUNITY_USER}/${IMAGE_NAME}-${PLATFORM}:${TAG}
-    OUT_TAGS="${OUT_TAGS} ${PUBLIC_REGISTRY_URL}/${PUBLIC_COMMUNITY_USER}/${IMAGE_NAME}-${PLATFORM}:${TAG}"
+
+  case $PLATFORM in
+  amd64)
+    ARCH =x86_64
+    ;;
+
+  arm64v8)
+    ARCH =arm64
+    ;;
+
+  arm32v7)
+    ARCH =arm
+    ;;
+  esac
+
+  LOGGER_RUN docker pull --platform ${PLATFORM} ${PUBLIC_REGISTRY_URL}/${PUBLIC_COMMUNITY_USER}/${IMAGE_NAME}-${ARCH}:${TAG}
+  OUT_TAGS="${OUT_TAGS} ${PUBLIC_REGISTRY_URL}/${PUBLIC_COMMUNITY_USER}/${IMAGE_NAME}-${ARCH}:${TAG}"
 done
 
 LOGGER_RUN docker manifest create ${PRIVATE_REGISTRY_URL}/${PRIVATE_COMMUNITY_USER}/${IMAGE_NAME}:${TAG} ${OUT_TAGS}
@@ -44,4 +57,3 @@ LOGGER_RUN docker manifest create ${PRIVATE_REGISTRY_URL}/${PRIVATE_COMMUNITY_US
 LOGGER_RUN docker manifest push ${PRIVATE_REGISTRY_URL}/${PRIVATE_COMMUNITY_USER}/${IMAGE_NAME}:${TAG}
 
 LOGGER_RUN docker manifest inspect ${PRIVATE_REGISTRY_URL}/${PRIVATE_COMMUNITY_USER}/${IMAGE_NAME}:${TAG}
-
