54806caee168103d0a5ceb62b43693b1d01c73b1
tencent-wechat <tencent-wechat@users.noreply.github.com>
2021-11-11 13:52:46 +0000

    修复和优化
    修复错误：打开非 user_status 的 json 文件可能造成问题。优化开发用脚本

--- docker-build-dev.sh
+++ docker-build-dev.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-# eg ./dev_build.sh https://github.com/TechXueXi/TechXueXi.git developing localhost:5000
+# eg ./docker-build-dev.sh https://github.com/TechXueXi/TechXueXi.git developing  localhost:5000 arm64v8 Dockerfile_arm64v8.dev
 
 USESOURCE='https://github.com/TechXueXi/TechXueXi.git'
 USEBRANCHE='developing'
 IMAGE_TAG="techxuexi/techxuexi-amd64:${USEBRANCHE}"
 PUSH_REGISTRY_URL='docker.io'
-
+DOCKER_FILE='Dockerfile.dev'
 if [[ $1 ]]; then
     USESOURCE=$1
 fi
 if [[ $2 ]]; then
-    USESOURCE=$2
+    USEBRANCHE=$2
 fi
 if [[ $3 ]]; then
     PUSH_REGISTRY_URL=$3
     IMAGE_TAG=${PUSH_REGISTRY_URL}/${IMAGE_TAG}
 fi
-
+if [[ $4 ]]; then
+    IMAGE_TAG="techxuexi/techxuexi-$4:${USEBRANCHE}"
+fi
+if [[ $5 ]]; then
+    DOCKER_FILE=$5
+fi
 set -x
 docker build \
-    -f Dockerfile.dev \
+    -f ${DOCKER_FILE} \
     --build-arg "usesource=${USESOURCE}" \
     --build-arg "usebranche=${USEBRANCHE}" \
     --tag "${IMAGE_TAG}" \
-    ${USESOURCE}
+    .
 
 docker push
