171cde839507c78b90ebb19dda87c012079f5a23
zytomorrow <z794672847@gmail.com>
2021-11-24 12:42:09 +0800

    :new: pushpllus 支持按群组推送

--- SourcePackages/pdlearn/pluspush.py
+++ SourcePackages/pdlearn/pluspush.py
@@ -1,8 +1,10 @@
 import json
 import requests  # 导入依赖库
 class PlusPushHandler:
-    def __init__(self, token):
+    def __init__(self, token, topic=''):
         self.token = token
+        self.topic = topic
+
     def ftmsgsend(self, msgurl):
            
             url="http://www.pushplus.plus/send"
@@ -21,7 +23,8 @@ class PlusPushHandler:
             data = {
                 "token":self.token,
                 "title": "学 xi 强 guo ",
-                "content":text
+                "content": text,
+                "topic": self.topic
             }
 
             res = requests.post(url, data=json.dumps(data), headers=headers)  # 发送post请求
