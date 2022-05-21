d896e46b1d2068ffc9add96a38d40de6a9fdd2b3
gituse4xxqg <gituse4xxqg@protonmail.com>
2022-05-21 16:13:53 +0800

    add anno [bug fixed] 当前六页学完以后，第七页的专项会找不到，ubuntu 20.04经过验证

--- SourcePackages/pdlearn/answer_question.py
+++ SourcePackages/pdlearn/answer_question.py
@@ -24,13 +24,21 @@ def generate_tiku_data(quiz_type=None, tip=None, option=None, answer=None, quest
 
 
 def find_available_quiz(quiz_type, driver_ans, uid):
-    pages = driver_ans.driver.find_elements_by_css_selector(
-        ".ant-pagination-item")
-    for p in range(0, len(pages), 1):  # (从最后一页开始往前找做题)从前往后找题，专项答题等没有那么离谱
-        time.sleep(0.5)
-        print('进入答题第' + str(p+1) + '页')
-        pages[p].click()
-        time.sleep(0.5)
+    # 这里有些问题，专项学习一开始会出现 1 2 3 4 5 10，一共6个button
+    # 第一次find_elements_by_css_selector以后只会学到前5页和最后一页，这6页学完以后就会找不到
+    # 这里改动先找最后一个button的页码，然后每次点击，更新pages，模拟手动点击的过程
+    prestr = ".ant-pagination-item"
+    pages = driver_ans.driver.find_elements_by_css_selector(prestr)
+    last = int(pages[len(pages)-1].text)
+    for p in range(0, last, 1):  # (从最后一页开始往前找做题)从前往后找题，专项答题等没有那么离谱
+        pages = driver_ans.driver.find_elements_by_css_selector(prestr)
+        for index in range(0, len(pages), 1):
+            if pages[index].text == str(p+1):
+                time.sleep(0.5)
+                print('进入答题第' + str(p+1) + '页')
+                pages[index].click()
+                time.sleep(0.5)
+                break
         dati = []
         if quiz_type == "weekly":  # 寻找可以做的题
             dati = driver_ans.driver.find_elements_by_css_selector(
